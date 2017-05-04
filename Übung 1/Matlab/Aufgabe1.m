576664b1acb15290c1fff2a0ebf80420c11d9021
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-05-04 15:44:46 +0200

    Implemented rolling average and gain for the Karplus Algortihm.

--- Übung 1/Matlab/Aufgabe1.m
+++ Übung 1/Matlab/Aufgabe1.m
@@ -9,12 +9,23 @@ f0 = 100;
 wavetable = Wavetable(fs, f0);
 
 % b)
+gain = 1.0;
 
-y1 = Karpus(fs, f0, 3.0);
+y1 = Karplus(fs, f0, 3.0, 1, gain);
 audiowrite('y_1.wav', y1, fs);
 
-y2 = Karpus(fs, f0, 3.0);
+y2 = Karplus(fs, f0, 3.0, 1, gain);
 audiowrite('y_2.wav', y2, fs);
 
-y3 = Karpus(fs, f0, 3.0);
+y3 = Karplus(fs, f0, 3.0, 1, gain);
 audiowrite('y_3.wav', y3, fs);
+
+%% c)
+% TODO: Matlab lutscht. Rausfinden wie spectrogramme gehen
+plot(abs(fft(y1)));
+
+
+%% Aufgabe 2
+gain = 0.9799;
+y = Karplus(fs, f0, 3.0, 2, gain);
+audiowrite('mean.wav', y, fs);
---
+++ Übung 1/Matlab/Karplus.m
@@ -0,0 +1,33 @@
+%
+% Karplus-Strong Algorithm
+%
+% fs          : Samplerate
+% f0          : Grundfrequenz
+% lengthInSec : Length of the output signal in seconds
+% averageN    : Number of elements the are averaged
+% gain        : Gain
+
+function [samples] = Karplus(fs, f0, lengthInSec, averageN, gain)
+    % generate wavetable and initialize stuff
+    wavetable = Wavetable(fs, f0);
+    wavetableLength = size(wavetable, 2);
+    sampleCount = fs * lengthInSec;
+    samples = zeros(1, sampleCount);
+    
+    for i = 0:(sampleCount - 1)
+        head = mod(i, wavetableLength) + 1;
+        
+        % calculate the rolling average
+        mean = wavetable(head);
+        for j = 1:(averageN - 1)
+            tail = mod(i + j, wavetableLength) + 1;
+            mean = mean + wavetable(tail);
+        end
+        mean = mean / averageN;
+        % modify the wavetable
+        wavetable(head) = mean * gain;
+        
+        % write output
+        samples(i + 1) = wavetable(head);
+    end
+end
