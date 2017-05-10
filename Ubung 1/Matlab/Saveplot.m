232f0f21427a54ab7b4b84d184e7f7ede0bf1ed2
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-05-10 22:08:02 +0200

    Saveplot function for matlab to pdf (latex) conversion

---
+++ Ubung 1/Matlab/Saveplot.m
@@ -0,0 +1,6 @@
+function [] = Saveplot(h, name)
+    set(h,'Units','Inches');
+    pos = get(h,'Position');
+    set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+    print(h,name,'-dpdf','-r0');
+end
