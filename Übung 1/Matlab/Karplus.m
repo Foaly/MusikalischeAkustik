a5c3257a99cbb42a9dbc3c72e13f9f1a4a6e59bd
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-05-10 18:44:32 +0200

    Added Wavetable animation.

---
+++ Übung 1/Matlab/Karplus.m
@@ -0,0 +1,44 @@
+%
+% Karplus-Strong Algorithm
+%
+% fs          : Samplerate
+% f0          : Fundamental frequency
+% lengthInSec : Length of the output signal in seconds
+% filter      : Filter coefficients
+% gain        : Gain
+% draw        : boolean, if the wavetable should be drawn
+
+function [samples] = Karplus(fs, f0, lengthInSec, filter, gain, draw)
+    % generate wavetable and initialize stuff
+    wavetable = Wavetable(fs, f0);
+    wavetableLength = size(wavetable, 2);
+    sampleCount = fs * lengthInSec;
+    samples = zeros(1, sampleCount);
+    averageN = size(filter, 2);
+    figure;
+
+    for i = 0:(sampleCount - 1)
+        head = mod(i, wavetableLength) + 1;
+        
+        % calculate the rolling average (only has an effect for 
+        % averageN > 1)
+        mean = wavetable(head);
+        for j = 1:(averageN - 1)
+            tail = mod(i + j, wavetableLength) + 1;
+            mean = mean + wavetable(tail) * filter(j);
+        end
+        mean = mean / averageN;
+        % modify the wavetable
+        wavetable(head) = mean * gain;
+        
+        % write output
+        samples(i + 1) = wavetable(head);
+        
+        if draw == true && head == 1
+            plot(wavetable);
+            ylim([-1 1]);
+            xlim([0 wavetableLength]);
+            pause(0.01);
+        end
+    end
+end
