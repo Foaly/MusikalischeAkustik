ca77f82eef34c131c172004dea51844e190b9e01
Jason <jason-horn@versanet.de>
2017-06-02 17:14:40 +0200

    did A1c)

--- Uebung02/Matlab/main.m
+++ Uebung02/Matlab/main.m
@@ -8,6 +8,18 @@ equalTemperamentScale = EqualTemperamentScale(16.3516);
 
 pythagoreanScale = PythagoreanScale(16.3516);
 
+%% c)
+
+figure;
+diff = [0 -10 4 -6 8 -2 +11 +2 -8 +6 -4 +10 0];
+bar(diff, 'k');
+ylim([-12 12]);
+xticks([1 2 3 4 5 6 7 8 9 10 11 12 13])
+xticklabels({'C','Des/Cis','D','Es/Dis','E','F','Ges/Fis', 'G', 'As/Gis', 'A', 'As/B', 'H', 'C'})
+ylabel('Unterschied in Cent');
+grid on;
+
+
 %% Aufgabe 2
 
 %% a)
