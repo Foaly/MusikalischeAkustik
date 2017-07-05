05faeec92644b810b95fa364c0ab6a79b3419ed9
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-07-05 14:22:00 +0200

    Init python

---
+++ Uebung04/Python/main.py
@@ -0,0 +1,18 @@
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import glob
+import itertools
+
+
+from sklearn.cluster import KMeans
+
+
+def main():
+    print("Blatt 4 - Sinusoidal Modeling")
+
+
+
+if __name__ == '__main__':
+    main()
